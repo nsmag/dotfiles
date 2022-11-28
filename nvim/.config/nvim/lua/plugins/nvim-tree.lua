15dfdf917079687a3cd2f6bb22f75a5963729bf4
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2022-11-28 14:41:51 +0700

    Update nvim-tree config

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,29 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvimtree.setup()
+nvimtree.setup({
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
+		icons = {
+			git_placement = "after",
+			glyphs = {
+				git = {
+					unstaged = "!",
+					staged = "+",
+					unmerged = "=",
+					renamed = "»",
+					untracked = "?",
+					deleted = "✗",
+				},
+			},
+		},
+	},
+})
