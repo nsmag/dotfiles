3f111d3e22970919861bc4bb64f6e6388acf033e
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2023-01-27 23:05:24 +0700

    No preselect

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,7 +5,7 @@ return {
 
     return {
       completion = {
-        completeopt = "menu,menuone,noinsert",
+        completeopt = "menu,menuone,noinsert,noselect",
       },
       snippet = {
         expand = function(args)
