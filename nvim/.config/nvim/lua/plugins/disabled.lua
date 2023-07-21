0ebba6b88cf1bd4e027f7d7db32de62e362f4900
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2023-07-21 16:12:37 +0700

    Enable gitsigns

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "lewis6991/gitsigns.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
 }
