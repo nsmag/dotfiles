ddd2c974d82fa5f21f6808d1fa0d5613c3dc4345
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2023-09-16 16:20:58 +0700

    Update nvim lsp keys

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -60,5 +60,9 @@ return {
         end,
       },
     },
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<c-k>", false }
+    end,
   },
 }
