97f4d13af85a69f175eb5295b3030d435a5c4105
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2024-03-26 17:36:06 +0700

    Add basic lua_ls config

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -25,6 +25,28 @@ return {
             },
           },
         },
+        lua_ls = {
+          setup = {
+            on_init = function(client)
+              local path = client.workspace_folders[1].name
+              if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+                return
+              end
+
+              client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                runtime = {
+                  version = "LuaJIT",
+                },
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME,
+                  },
+                },
+              })
+            end,
+          },
+        },
         tailwindcss = {
           filetypes_exclude = { "markdown" },
           settings = {
