5f1c66aba2be226540c97a87077fac0a25354d33
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2024-03-28 14:10:44 +0700

    Disable audible bell

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,10 +2,11 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.window_decorations = "RESIZE"
 config.adjust_window_size_when_changing_font_size = false
-config.hide_tab_bar_if_only_one_tab = true
+config.audible_bell = "Disabled"
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Hack Nerd Font")
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
 return config
