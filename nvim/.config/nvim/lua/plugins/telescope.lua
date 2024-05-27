f0a05f66d89cc237228c1b66aa4a6cc2a6f62244
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2024-05-28 00:20:43 +0700

    update nvim config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,15 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = function()
+  opts = function(_, opts)
     local actions = require("telescope.actions")
 
-    return {
-      defaults = {
-        mappings = {
-          i = {
-            ["<c-j>"] = actions.move_selection_next,
-            ["<c-k>"] = actions.move_selection_previous,
-          },
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+      mappings = {
+        i = {
+          ["<c-j>"] = actions.move_selection_next,
+          ["<c-k>"] = actions.move_selection_previous,
         },
       },
-    }
+    })
   end,
 }
