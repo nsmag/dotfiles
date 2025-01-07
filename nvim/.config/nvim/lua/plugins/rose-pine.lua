c5c3df04819f1d47f1903749498a69d45f9c7ec7
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2025-01-07 17:09:13 +0700

    Transparent background

--- nvim/.config/nvim/lua/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/plugins/rose-pine.lua
@@ -3,6 +3,9 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     opts = {
+      styles = {
+        transparency = true,
+      },
       highlight_groups = {
         SnacksIndent = { fg = "overlay" },
         SnacksIndentScope = { fg = "muted" },
