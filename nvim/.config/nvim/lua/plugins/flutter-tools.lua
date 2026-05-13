f5c2d4bf8cb0c1e59cff7224154074f6a4f0b047
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2026-05-13 12:50:38 +0700

    Update flutter-tools in nvim

--- nvim/.config/nvim/lua/plugins/flutter-tools.lua
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -1,23 +1,11 @@
 return {
   {
-    "akinsho/flutter-tools.nvim",
+    "nvim-flutter/flutter-tools.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim",
     },
     config = true,
-    opts = {
-      dev_tools = {
-        autostart = true,
-      },
-      lsp = {
-        color = {
-          enabled = true,
-          background = true,
-          virtual_text = false,
-        },
-      },
-    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
