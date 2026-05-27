22c05b5e65f0301dc455c91b3debdf868e934a6a
Natthakit Susanthitanon <natthakit.susanthitanon@gmail.com>
2026-05-27 17:44:10 +0700

    Suppress some warnings from marksman

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,19 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     setup = {
+      marksman = function(_, opts)
+        opts.handlers = {
+          ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
+            if result and result.diagnostics then
+              result.diagnostics = vim.tbl_filter(function(d)
+                return not (d.message:find('Ambiguous link', 1, true)
+                         or d.message:find('Duplicate definition', 1, true))
+              end, result.diagnostics)
+            end
+            vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
+          end,
+        }
+      end,
       tailwindcss = function(_, opts)
         opts.settings = {
           tailwindCSS = {
